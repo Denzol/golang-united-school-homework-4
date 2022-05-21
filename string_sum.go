4ac850ce474c95f5f8efd3e298243a7f62ca4cd5
Hramtsov Denis <denis.khramtsov@mail.ru>
2022-05-21 21:48:48 +1000

    my version

--- string_sum.go
+++ string_sum.go
@@ -2,26 +2,35 @@ package string_sum
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
-
-//use these errors as appropriate, wrapping them with fmt.Errorf function
-var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
-	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-)
-
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	slice := strings.Split(input, "")
+	if len(slice) < 3 || len(slice) > 4 {
+		var errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+		return "", errorNotTwoOperands
+	}
+	var slice2 []string
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == "-" {
+			k := slice[i] + slice[i+1]
+			i++
+			slice2 = append(slice2, k)
+		} else {
+			slice2 = append(slice2, slice[i])
+		}
+	}
+	sum := 0
+	for _, n := range slice2 {
+		k, _ := strconv.Atoi(n)
+		sum = sum + k
+	}
+	if sum == 0 {
+		var errorEmptyInput = errors.New("input is empty")
+		output = ""
+		return output, errorEmptyInput
+	}
+	return strconv.Itoa(sum), nil
 }
